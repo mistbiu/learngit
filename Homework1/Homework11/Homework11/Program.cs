a5820dfaf4afe579a8ec2c0817431289c2b16c8a
mistbiu <746078047@qq.com>
2019-02-28 13:34:27 +0800

    1 time for homework1

---
+++ Homework1/Homework11/Homework11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Product
+{
+    public static void Main(string[] args)
+    {
+        string s = "";
+        int n1 = 0;
+        int n2 = 0;
+        Console.Write("Input the first number:");
+        s = Console.ReadLine();
+        n1 = Int32.Parse(s);
+        Console.Write("Input the second number:");
+        s = Console.ReadLine();
+        n2 = Int32.Parse(s);
+        n1 = n1 * n2;
+        Console.WriteLine("The result is " + n1 + " .");
+    }
+}
---
+++ Homework1/Homework12/Homework12/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = ""; 
+            s  = textBox1.Text;
+            int n1 = int.Parse(s);
+            s = textBox2.Text;
+            int n2 = int.Parse(s);
+            int n3 = n1 * n2;
+            textBox3.Text = n3.ToString();
+
+        }
+
+        
+    }
+}
