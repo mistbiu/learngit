b7e15a1eed237c9891763d03929cd5ed2c73d682
mistbiu <746078047@qq.com>
2019-03-12 16:00:57 +0800

    homework3 done

---
+++ Homework3/Calculator/Calculator/Program.cs
@@ -0,0 +1,120 @@
+﻿/*参考抽象工厂模式，程序实现加减乘除运算。*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public abstract class Calculate
+    {
+        private string myOp;
+        public Calculate(string s)
+        {
+            myOp = s;
+        }
+        public string Op
+        {
+            get
+            {
+                return myOp;
+            }
+            set
+            {
+                myOp = value;
+            }
+        }
+        public abstract double Result
+        {
+            get;
+        }
+        public override string ToString()
+        {
+            return Op + " = " + Result.ToString("#0.00");
+        }
+    }
+    public class Add : Calculate
+    {
+        private int num1, num2;
+        public Add(int n1,int n2,string op):base(op)
+        {
+            num1 = n1;
+            num2 = n2;
+        }
+        public override double Result
+        {
+            get
+            {
+                return num1 + num2;
+            }
+        }
+    }
+    public class Sub : Calculate
+    {
+        private int num1, num2;
+        public Sub(int n1, int n2, string op) : base(op)
+        {
+            num1 = n1;
+            num2 = n2;
+        }
+        public override double Result
+        {
+            get
+            {
+                return num1 - num2;
+            }
+        }
+    }
+    public class Mul : Calculate
+    {
+        private int num1, num2;
+        public Mul(int n1, int n2, string op) : base(op)
+        {
+            num1 = n1;
+            num2 = n2;
+        }
+        public override double Result
+        {
+            get
+            {
+                return num1 * num2;
+            }
+        }
+    }
+    public class Div : Calculate
+    {
+        private int num1, num2;
+        public Div(int n1, int n2, string op) : base(op)
+        {
+            num1 = n1;
+            num2 = n2;
+        }
+        public override double Result
+        {
+            get
+            {
+                return (double)num1/(double)num2;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate[] cals =
+            {
+                new Add(34,23,"34 add 23 "),
+                new Sub(32, 14, "32 sub 14 "),
+                new Mul(6, 17, "6 mul 17 "),
+                new Div(62, 4, "62 div 4 ")
+            };
+            Console.WriteLine("Calcutator working");
+            foreach(Calculate c in cals)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
---
+++ Homework3/GradesReward/GradesReward/Program.cs
@@ -0,0 +1,91 @@
+﻿/*参考简单工厂模式，程序实现：输入一个分数，判断分数为ABCD哪一等级，并给出奖励。*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public interface Grade
+{
+      void reward();
+}
+class LevelA : Grade
+{
+    public LevelA()
+    {
+        Console.WriteLine("Level A!");
+    }
+    public void reward()
+    {
+        Console.WriteLine("Reward a backpack!");
+    }
+}
+class LevelB : Grade
+{
+    public LevelB()
+    {
+        Console.WriteLine("Level B!");
+    }
+    public void reward()
+    {
+        Console.WriteLine("Reward a book!");
+    }
+}
+class LevelC : Grade
+{
+    public LevelC()
+    {
+        Console.WriteLine("Level C!");
+    }
+    public void reward()
+    {
+        Console.WriteLine("Reward a pen!");
+    }
+}
+class LevelD : Grade
+{
+    public LevelD()
+    {
+        Console.WriteLine("Level D!");
+    }
+    public void reward()
+    {
+        Console.WriteLine("No reward! Keep fighting!");
+    }
+}
+class GradeJudge
+{
+    public static Grade GetGrade(int score)
+    {
+        Grade grade = null;
+        if (score >= 90)
+        {
+            grade = new LevelA();
+        }
+        else if (score >= 80 && score < 90)
+        {
+            grade = new LevelB();
+        }
+        else if (score >= 70 && score < 80)
+        {
+            grade = new LevelC();
+        }
+        else
+        {
+            grade = new LevelD();
+        }
+        return grade;
+    }
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input your score:");
+        int score =int.Parse ( Console.ReadLine());
+        Grade grade = GradeJudge.GetGrade(score);
+        grade.reward();
+    }
+}
