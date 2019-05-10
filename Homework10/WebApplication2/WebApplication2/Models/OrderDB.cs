9048fe1507bdcd6d3ff7933206d2c977302c2b80
mistbiu <746078047@qq.com>
2019-05-10 12:29:43 +0800

    Homework10

---
+++ Homework10/WebApplication2/WebApplication2/Models/OrderDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class OrderDB : DbContext
+    {
+        public OrderDB()
+            : base("OrderDataBase")
+        {
+        }
+        public DbSet<Order> tb_Order { get; set; }
+
+        public DbSet<OrderDetails> tb_OrderDetails { get; set; }
+    }
+}
---
+++ Homework10/WebApplication2/WebApplication2/Models/program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            OrderService orderService = new OrderService();
+
+            OrderDetails details1 = new OrderDetails("1", "beats", 2, 1099);
+            OrderDetails details2 = new OrderDetails("2", "xiaomi", 1, 1999);
+            OrderDetails details3 = new OrderDetails("3", "iphone", 4, 6999);
+
+            List<OrderDetails> items1 = new List<OrderDetails> { details1, details2 };
+            List<OrderDetails> items2 = new List<OrderDetails> { details3 };
+
+            Order order1 = new Order("100001", "fafa", "13647118626", items1);
+            Order order2 = new Order("100002", "lulu", "13886318833", items2);
+
+
+            orderService.AddOrder(order1);
+            orderService.AddOrder(order2);
+        }
+    }
+}
